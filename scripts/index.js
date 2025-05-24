1b14214a5ca9950416f9ade777581d204e226f23
Anthony Muro <Anthonymuro94@icloud.com>
2025-05-24 12:00:48 -0700

    added inital card in js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,36 @@
+const initialCards = [
+  {
+    name: "Val Thorens",
+    // URL of the image representing the card
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    // URL of the image representing the card
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    // URL of the image representing the card
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    // URL of the image representing the card
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    // URL of the image representing the card
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    // URL of the image representing the card
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
 // edit profile modal
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
@@ -63,7 +96,14 @@ function handleAddCardSubmit(evt) {
   evt.preventDefault();
   console.log("Card Name:", nameInput.value);
   console.log("Card Link:", linkInput.value);
+
+  //modal is opened
   addCardModal.classList.remove("modal_is-opened");
 }
 
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+initialCards.forEach(function (item) {
+  consolelog(item.name);
+  console.log(item.link);
+});
